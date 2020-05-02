a8e80f10c24d28ce2cab5770c45b9ff9dcb68afb
josedev03 <joseing03@gmail.com>
2020-05-02 17:42:47 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # prosuma
+
+paquete de pruebas
---
+++ index.js
@@ -0,0 +1,10 @@
+function prosaludo(mensaje, destinatario = ''){
+    let mensaje = ``;
+    mesnaje += destinatario ? `Apreciad@ ${destinatario} usted ha recibido el siguiente mensaje ` : ''
+    mensaje += `${mensale}`
+    console.log(mensaje)
+}
+
+module.exports = {
+    prosaludo
+}
